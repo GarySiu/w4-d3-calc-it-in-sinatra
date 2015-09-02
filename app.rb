6fdbec949f60403c5f1849808fd347d17212ac13
Gary Siu <Gary@untypeable.com>
2015-09-02 19:19:52 +0100

    Lets start again with a forms and posting data instead

--- app.rb
+++ app.rb
@@ -6,9 +6,12 @@ get '/' do
   erb :home
 end
 
-get '/:first/:op/:second' do
-  @first = params[:first]
-  @op = params[:op]
-  @second = params[:second]
-  erb :home
-end
+
+#         BAD IDEA
+#
+# get '/:first/:op/:second' do
+#   @first = params[:first]
+#   @op = params[:op]
+#   @second = params[:second]
+#   erb :home
+# end
