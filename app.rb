5cd9556fd1d48f5f5033c6e280b604592052e019
Gary Siu <Gary@untypeable.com>
2015-09-02 20:05:24 +0100

    replicated files for all the calculators and set up the routing

--- app.rb
+++ app.rb
@@ -10,3 +10,30 @@ post '/' do
 # binding.pry
   erb :home
 end
+
+get '/bmi' do
+  erb :bmi
+end
+
+post '/bmi' do
+# binding.pry
+  erb :bmi
+end
+
+get '/mortgage' do
+  erb :mortgage
+end
+
+post '/mortgage' do
+# binding.pry
+  erb :mortgage
+end
+
+get '/trip' do
+  erb :trip
+end
+
+post '/trip' do
+# binding.pry
+  erb :trip
+end
