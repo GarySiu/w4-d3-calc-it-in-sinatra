011ee937c6629e90998ff08cdff95560c364cd83
Gary Siu <Gary@untypeable.com>
2015-09-03 09:47:12 +0100

    Refactored basic calculator a bit based on hw review

--- models/basic.rb
+++ models/basic.rb
@@ -1,17 +1,13 @@
 class Basic
   
   def self.calc(num1,op,num2)
-    case op
-      when '+'
-        result = num1.to_f + num2.to_f
-      when '-'
-        result = num1.to_f - num2.to_f
-      when '*'
-        result = num1.to_f * num2.to_f
-      when '/'
-        result = num1.to_f / num2.to_f
+    result = case op
+      when '+' then num1.to_f + num2.to_f
+      when '-' then num1.to_f - num2.to_f
+      when '*' then num1.to_f * num2.to_f
+      when '/' then num1.to_f / num2.to_f
       else
-        result = 'Please enter numbers into the calculator'
+        'Please enter numbers into the calculator'
     end
 
     result.to_i == result ? result = result.to_i : result #You have so many methods but not this? Why Ruby why?
